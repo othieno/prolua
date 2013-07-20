5c445f512004f59254ba184fde5988305f999c74
supranove <j.othieno@gmail.com>
2013-07-20 22:22:36 +0200

    Updated lua2prolog.lua to load a program's abstract syntax tree.

--- src/lua2prolog.lua
+++ src/lua2prolog.lua
@@ -20,4 +20,57 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 -- THE SOFTWARE.
 
-print("program([s0, s1, s2, s3, sn]).");
+-- lua2prolog is inspired by https://github.com/davidm/lua2c/blob/master/lua2c.lua
+-- written by David Manura. It will convert the abstract syntax tree of a Lua
+-- program into a Prolog format that can be interpreted by Prolua.
+
+local _G      = _G
+local assert  = _G.assert
+local error   = _G.error
+local io      = _G.io
+local ipairs  = _G.ipairs
+local os      = _G.os
+local package = _G.package
+local require = _G.require
+local string  = _G.string
+local table   = _G.table
+
+-- Include parsing libraries taken from Metalua.
+package.path = package.path .. ";./metalua/?.lua"
+
+require "lexer"
+require "gg"
+require "mlp_lexer"
+require "mlp_misc"
+require "mlp_table"
+require "mlp_meta"
+require "mlp_expr"
+require "mlp_stat"
+require "mlp_ext"
+_G.mlc = {} -- make gg happy
+local mlp = assert(_G.mlp)
+
+-- Load input file.
+local filename = ...;
+if not filename then
+  io.stderr:write("usage: lua2prolog filename.lua\n");
+  os.exit(1);
+end
+local file = assert(io.open (filename, 'r'));
+local input = file:read "*a";
+file:close();
+input = input:gsub("^#[^\r\n]*", "") -- remove any shebang
+
+-- Get the program's abstract syntax tree (AST).
+local lx = mlp.lexer:newstream(input);
+local AST = mlp.chunk(lx);
+
+-- This function converts an AST's node into Prolog, in a format that
+-- can be parsed and interpreted by Prolua.
+function ASTNodeToProlog(ASTNode)
+	local Prolog = "s0, s1, s2, s3, sn";
+	return Prolog;
+end
+
+-- Recursively convert the root node into Prolog.
+print("program(["..ASTNodeToProlog(AST).."]).");
