95702bc578276dea41e481fec4d5b6b465e42bda
supranove <j.othieno@gmail.com>
2013-09-05 17:36:31 +0200

    Added tests for closures.

--- samples/closures.lua
+++ samples/closures.lua
@@ -21,17 +21,44 @@
 -- THE SOFTWARE.
 
 -- A simple function to return the square of a value.
-function square(x)
-	return x * x;
-end;
-
--- A function to calculate the factorial of a given value.
-factorial = function(n)
-	if (n > 0) then
-		return n * factorial(n - 1)
-	else
-		return 1
-	end
+local function square(x)
+   return x * x
 end
 
-return square(26), factorial(6);
+-- A table where function objects will be stored.
+local math = {}
+
+-- A function to calculate the factorial of a given value.
+math.factorial = function(n)
+   if n > 0 then
+      return n * math.factorial(n - 1)
+   else
+      return 1
+   end
+end
+
+
+-- A naive implementation to calculate the fibonacci number.
+-- Warning, Prolua takes time calculating values of n greater than 13.
+-- This is a good function to test Prolua's inefficiency (pay attention to
+-- the number of execution context graph nodes).
+math["fibonacci"] = function(n)
+   if n < 2 then
+      return n
+   else
+      return math["fibonacci"](n - 1) + math["fibonacci"](n - 2)
+   end
+end
+
+
+-- A simple function to test closures.
+-- Taken from http://en.wikipedia.org/wiki/Lua_%28programming_language%29#Functions
+function addto(x)
+   return function(y)
+      return x + y
+   end
+end
+fourplus = addto(4)
+
+
+return square(26), math.factorial(256), math.fibonacci(8), fourplus(3)
